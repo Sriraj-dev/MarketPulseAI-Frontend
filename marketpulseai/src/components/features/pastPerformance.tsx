aea4d84bcc57ec712cb63e95ce6cd3bccb31202f
Harmeet135 <harmeetchahal9891@gmail.com>
2025-01-26 02:42:36 +0530

    build error fix

--- marketpulseai/src/components/features/pastPerformance.tsx
+++ marketpulseai/src/components/features/pastPerformance.tsx
@@ -1,10 +1,17 @@
 'use client';
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import ApexChart from './candle';
 import 'swiper/css';
 import 'swiper/css/navigation'; // Import navigation styles
 import { Navigation , FreeMode } from 'swiper/modules';
+
+import dynamic from 'next/dynamic';
+
+// Dynamically import ApexChart with ssr disabled
+const ApexChart = dynamic(() => import("@/components/features/candle"), {
+  ssr: false,
+});
+
 
 const PastPerformance = () => {
   return (
@@ -12,8 +19,8 @@ const PastPerformance = () => {
       <h3 className="text-lg font-semibold mt-4 mb-2">Recommendations</h3>
       <Swiper
       freeMode={false}
-        navigation={true} // Enable navigation arrows
-        modules={[Navigation , FreeMode]} // Pass Navigation module
+        navigation={true}
+        modules={[Navigation , FreeMode]} 
         className="mySwiper"
         spaceBetween={20}
         slidesPerView={1}
